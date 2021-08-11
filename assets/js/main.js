83dbc74c2413e546f6f81a6b7b574c9a825b9ca6
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-08-11 19:12:26 +0200

    In assets/img/ folder, updated the work1.jpg, work2.jpg, work3.jpg, work4.jpg, work5.jpg files. In index.html file, lines 12-17: added the <link> tag with the stylesheet for the swiper-bundle.min.css file and replaced the <link> tag with styles.css, line 119: change the '.bd-grid' class by the 'grid' class, line 192: updated the src attribute for the <img> tag by the work6.jpg file, lines 201-293: change the structure html of the work part for use the swiper js library, lines 324-327: added the <script> tag for use the swiper js library but problem with one error 404 with the src attribute contains the swiper-bundle.min.js file so use the cdn of the swiper. In assets/css/ folder, for the styles.css file, deleted the property display with the block value for the img selector, lines 335-337: added the '.button--small' selector, lines 421-480: deleted the old selectors for the work part and added the new selectors and its properties for handling the css mode of swiper. In assets/css/ folder, added swiper-bundle.min.css file. In assets/js/ folder, for the main.js file, lines 29-43: initialize Swiper, lines 60,61 updated the selectors use the reveal and deleted for the '.work__img' selector.

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,7 +26,23 @@ function linkAction() {
 
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
+/*===== WORK SWIPER =====*/
+let swiperWork = new Swiper(".mySwiper", {
+    cssMode: true,
+    loop: true,
+
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
 /*===== SCROLL REVEAL ANIMATION =====*/
+
 const sr = ScrollReveal({
     origin: 'top',
     distance: '80px',
@@ -42,17 +58,14 @@ sr.reveal('.home__social-icon', {interval: 200})
 
 /*SCROLL ABOUT*/
 sr.reveal('.about__img', {})
-sr.reveal('.about__subtitle', {delay: 200})
-sr.reveal('.about__text', {delay: 400})
+sr.reveal('.about__description', {delay: 200})
+sr.reveal('.about__buttons', {delay: 400})
 
 /*SCROLL SKILLS*/
 sr.reveal('.skills__subtitle', {})
 sr.reveal('.skills__text', {delay: 200})
 sr.reveal('.skills__data', {interval: 200})
 sr.reveal('.skills__img', {delay: 400})
-
-/*SCROLL WORK*/
-sr.reveal('.work__img', {interval: 200})
 
 /*SCROLL CONTACT*/
 sr.reveal('.contact__form', {interval: 200})
