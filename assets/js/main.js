f9c8efb34a850ad5c510214bacfcf438fed1132c
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-08-12 12:17:30 +0200

    Added the scroll up handling, in the index.html file lines 368-371 then in the assets/css/styles.css file, lines 551-577 and the assets/js/main.js, lines 73-79. In the index.html file lines 374-375: commented the <script> tag with the swiper-bundle.min.js file in the cdn and decommented the <script> tag with the swiper-bundle.min.js file in the assets/js folder.

--- assets/js/main.js
+++ assets/js/main.js
@@ -69,3 +69,13 @@ sr.reveal('.skills__img', {delay: 400})
 
 /*SCROLL CONTACT*/
 sr.reveal('.contact__form', {interval: 200})
+
+/*===== SCROLL UP =====*/
+function scrollUp() {
+  const scrollUp = document.getElementById('scroll-up');
+  // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+  if(this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+
