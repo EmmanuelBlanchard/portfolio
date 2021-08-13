6ff40ea07734c528a9f0673f0345b67b1897d774
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-08-13 22:55:09 +0200

    In index.html file, lines 330-375: change the structure html of the contact part. In assets/css/ folder, in the styles.css file, lines 539,554,555,557-608: added the position property with the value relative to the selector '.contact__content' then updated the border property of the selector '.contact__input' by the '.15rem solid transparent' value and added the border-radius property with the '.5rem' value and added the padding property with the '.5rem' value, then added the '.contact__form.grid', '.contact__inputs.grid', '.contact__content input:focus', '.contact__content textarea:focus', '.contact__content.success input', .contact__content.error input', '.contact__content i', '.contact__content.success i.bx-check-circle', '.contact__content.error i.bx-error-circle', '.contact__content small', '.contact__content.error small' selectors for the handling displaying error or success of the inputs in the form. In assets/js/ folder,  in the main.js file, lines 52-249: added the handling displaying error or success of the inputs in the form.

--- assets/js/main.js
+++ assets/js/main.js
@@ -49,4 +49,201 @@ function scrollUp() {
 }
 window.addEventListener('scroll', scrollUp)
 
+/*===== FORM VALIDATION =====*/
+window.onload = () => {
+    lastname.addEventListener('input', checkInputLastName);
+    firstname.addEventListener('input', checkInputFirstName);
+    email.addEventListener('input', checkInputEmail);
+    message.addEventListener('input', checkInputMessage);
+    agreeterms.addEventListener('input', checkInputAgreeTerms);
+}
 
+let form = document.querySelector('#contact__form');
+const lastname = document.querySelector('#contact__form-lastname');
+const firstname = document.querySelector('#contact__form-firstname');
+const email = document.querySelector('#contact__form-email');
+const message = document.querySelector('#contact__form-message');
+const agreeterms = document.querySelector('#contact__form-agreeterms');
+
+function checkInputLastName() {
+    // trim to remove the whitespaces
+    const lastnameValue = lastname.value.trim();
+
+    if (lastnameValue === '') {
+        setErrorFor(lastname, 'Le nom ne peut pas être vide');
+    } else if (isLastNameNumber(lastnameValue)) {
+        setErrorFor(lastname, 'Le nom ne peut pas contenir un nombre');
+    } else if (!isLastName(lastnameValue)) {
+        setErrorFor(lastname, 'La première lettre doit être en majuscule');
+    } else {
+        setSuccessFor(lastname);
+    }
+}
+function checkInputFirstName() {
+    // trim to remove the whitespaces
+    const firstnameValue = firstname.value.trim();
+
+    if (firstnameValue === '') {
+        setErrorFor(firstname, 'Le prénom ne peut pas être vide');
+    } else if (isFirstNameNumber(firstnameValue)) {
+        setErrorFor(firstname, 'Le prénom ne peut pas contenir un nombre ');
+    } else if (!isFirstName(firstnameValue)) {
+        setErrorFor(firstname, 'La première lettre doit être en majuscule');
+    } else {
+        setSuccessFor(firstname);
+    }
+}
+function checkInputEmail() {
+    // trim to remove the whitespaces
+    const emailValue = email.value.trim();
+
+    if (emailValue === '') {
+        setErrorFor(email, 'L\'adresse e-mail ne peut pas être vide');
+    } else if (!isEmail(emailValue)) {
+        setErrorFor(email, 'L\'adresse e-mail n\'est pas valide');
+    } else {
+        setSuccessFor(email);
+    }
+}
+function checkInputMessage() {
+    // trim to remove the whitespaces
+    const messageValue = message.value.trim();
+    if (messageValue === '') {
+        setErrorFor(message, 'Le message ne peut pas être vide');
+    } else if (!isMessageLengthMin(messageValue)) {
+        setErrorFor(message, 'Un minimun 150 caractères est requis.');
+    } else if (!isMessageLengthMax(messageValue)) {
+        setErrorFor(message, 'Pas plus de 2000 caractères possible.');
+    } else {
+        setSuccessFor(message);
+    }
+}
+function checkInputAgreeTerms() {
+    const agreetermsValue = agreeterms.checked;
+    if (agreetermsValue === false) {
+        setErrorFor(agreeterms, 'La case doit être cochée pour envoyer le message.');
+    } else {
+        setSuccessFor(agreeterms);
+    }
+}
+
+form.addEventListener('submit', event => {
+    //e.preventDefault();
+    checkInputs(event);
+});
+
+function checkInputs(event) {
+    // trim to remove the whitespaces
+    const lastnameValue = lastname.value.trim();
+    const firstnameValue = firstname.value.trim();
+    const emailValue = email.value.trim();
+    const messageValue = message.value.trim();
+    const agreetermsValue = agreeterms.checked;
+    // We initialize the score
+    let score = 0;
+
+    if (lastnameValue === '') {
+        setErrorFor(lastname, 'Le nom ne peut pas être vide');
+    } else if (isLastNameNumber(lastnameValue)) {
+        setErrorFor(lastname, 'Le nom ne peut pas contenir un nombre');
+    } else if (!isLastName(lastnameValue)) {
+        setErrorFor(lastname, 'La première lettre doit être en majuscule');
+    } else {
+        score++;
+        setSuccessFor(lastname);
+    }
+
+    if (firstnameValue === '') {
+        setErrorFor(firstname, 'Le prénom ne peut pas être vide');
+    } else if (isFirstNameNumber(firstnameValue)) {
+        setErrorFor(firstname, 'Le prénom ne peut pas contenir un nombre ');
+    } else if (!isFirstName(firstnameValue)) {
+        setErrorFor(firstname, 'La première lettre doit être en majuscule');
+    } else {
+        score++;
+        setSuccessFor(firstname);
+    }
+
+    if (emailValue === '') {
+        setErrorFor(email, 'L\'adresse e-mail ne peut pas être vide');
+    } else if (!isEmail(emailValue)) {
+        setErrorFor(email, 'L\'adresse e-mail n\'est pas valide');
+    } else {
+        score++;
+        setSuccessFor(email);
+    }
+
+    if (messageValue === '') {
+        setErrorFor(message, 'Le message ne peut pas être vide');
+    } else if (!isMessageLengthMin(messageValue)) {
+        setErrorFor(message, 'Un minimun 150 caractères est requis.');
+    } else if (!isMessageLengthMax(messageValue)) {
+        setErrorFor(message, 'Pas plus de 2000 caractères possible.');
+    } else {
+        score++;
+        setSuccessFor(message);
+    }
+
+    if (agreetermsValue === false) {
+        setErrorFor(agreeterms, 'Cochez la case.');
+    } else {
+        score++;
+        setSuccessFor(agreeterms);
+    }
+
+    if (score === 5) {
+        alert('submit, Formulaire OK , Envoi en Cours !');
+        console.log("Formulaire OK , Envoi en Cours !");
+        event.preventDefault();
+        return true;
+    } else {
+        alert('submit, Erreur Formulaire!');
+        console.log("Erreur Formulaire!");
+        event.preventDefault();
+        return false;
+    }
+}
+
+function setErrorFor(input, message) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small');
+    formControl.className = 'contact__content error';
+    small.innerText = message;
+}
+
+function setSuccessFor(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'contact__content success';
+}
+
+function isLastNameNumber(lastname) {
+    return /\d/.test(lastname);
+}
+
+function isLastName(lastname) {
+    return /^[A-ZÀÂÄÇÉÈÊËÎÏÔÖÙÛÜŸÆŒ]{1}([a-zàâäçéèêëîïôöùûüÿæœðó])*[-'’\s]{0,1}([A-ZÀÂÄÇÉÈÊËÎÏÔÖÙÛÜŸÆŒ]{0,1}([a-zàâäçéèêëîïôöùûüÿæœðó]*)[-'’\s]{0,1})*$/.test(lastname);
+}
+
+function isFirstNameNumber(firstname) {
+    return /\d/.test(firstname);
+}
+
+function isFirstName(firstname) {
+    return /^[A-ZÀÂÄÇÉÈÊËÎÏÔÖÙÛÜŸÆŒ]{1}([a-zàâäçéèêëîïôöùûüÿæœðó])*[-'’\s]{0,1}([A-ZÀÂÄÇÉÈÊËÎÏÔÖÙÛÜŸÆŒ]{0,1}([a-zàâäçéèêëîïôöùûüÿæœðó]*)[-'’\s]{0,1})*$/.test(firstname);
+}
+
+function isMessageLengthMin(message) {
+    if (message.length >= 150) {
+        return true;
+    }
+}
+
+function isMessageLengthMax(message) {
+    if (message.length <= 2000) {
+        return true;
+    }
+}
+
+function isEmail(email) {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
