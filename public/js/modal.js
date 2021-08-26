c47ce4765805a9a1c45b0ad8e2eb1548c80cc3c2
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-08-26 21:36:11 +0200

    Removal of the legal notice modal with the use of the bootstrap framework because it uses selectors with properties that disfigure the layout with min-width: 360px so creating the modal from html, css and js.

---
+++ public/js/modal.js
@@ -0,0 +1,39 @@
+/* ===== LEGAL NOTICES MODAL =====*/
+window.onload = () => {
+    // We retrieve all the opening buttons of the modal
+    const modalButtons = document.querySelectorAll("[data-toggle=modal]");
+
+    for(let button of modalButtons) {
+        button.addEventListener("click", function(e) {
+            // We stop the navigation
+            e.preventDefault();
+            // We get the data-target
+            let target = this.dataset.target
+            // Retrieve the correct modal
+            let modal = document.querySelector(target);
+            // Display the modal window
+            modal.classList.add("show");
+
+            // We get the closing buttons
+            const modalClose = modal.querySelectorAll("[data-dismiss=dialog]");
+            console.log(modalClose)
+
+            for(let close of modalClose) {
+                close.addEventListener("click", () => {
+                    // We delete the modal window
+                    modal.classList.remove("show");
+                });
+            }
+
+            // We handle the closing of the modal window when clicking on the gray area
+            modal.addEventListener("click", function(){
+                this.classList.remove("show");
+            });
+            // We avoid the propagation of the click from a child to its parent
+            modal.children[0].addEventListener("click", function(e) {
+                // We stop the propagation
+                e.stopPropagation();
+            })
+        });
+    }
+}
