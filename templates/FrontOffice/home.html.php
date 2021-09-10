d26fc8f364547f0f73f938327393b81b0f2514cf
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-09-10 21:51:08 +0200

    Fix the error of the validator W3C HTML, Error: Attribute mask-type not allowed on element mask at this point, in the templates/FrontOffice/ folder and the home.html.php file(line 12: deleted the attribute mask-type) then in the public/css/ folder and the styles.css file(lines 321-324: added the mask selector with the mask-type property and its alpha value.). Fix the errors of the validator W3C HTML, Error: Element image is missing required attribute height and Error: Element image is missing required attribute width, in the in the templates/FrontOffice/ folder and the home.html.php file(line 24: added height and width attributes to the <image> tag.) Fix the error of the validator W3C HTML, Error: Bad value 0 for attribute cols on element textarea: Zero is not a positive integer, in the in the templates/FrontOffice/ folder and the home.html.php file(line 367: updated the cols attribut of the textarea tag with its 1 value.).

--- templates/FrontOffice/home.html.php
+++ templates/FrontOffice/home.html.php
@@ -9,7 +9,7 @@
 
             <div class="home__img">
                 <svg class="home__blob" viewBox="0 0 200 187" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                    <mask id="mask0" mask-type="alpha">
+                    <mask id="mask0">
                         <path d="M190.312 36.4879C206.582 62.1187 201.309 102.826 182.328 134.186C163.346 165.547 
                         130.807 187.559 100.226 186.353C69.6454 185.297 41.0228 161.023 21.7403 129.362C2.45775 
                         97.8511 -7.48481 59.1033 6.67581 34.5279C20.9871 10.1032 59.7028 -0.149132 97.9666 
@@ -21,7 +21,7 @@
                         129.362C2.45775 97.8511 -7.48481 59.1033 6.67581 34.5279C20.9871 10.1032 59.7028 
                         -0.149132 97.9666 0.00163737C136.23 0.303176 174.193 10.857 190.312 36.4879Z"/>
 
-                        <image class="home__blob-img" x='20' y='15' href="img/profil.png"/>
+                        <image class="home__blob-img" x='20' y='15' width="170px" height="170px" href="img/profil.png"/>
                     </g>
                 </svg>
             </div>
@@ -364,7 +364,7 @@
                 <i class='bx bx-check-circle' ></i>
                 <i class='bx bx-error-circle' ></i>
                 <label for="contact__form-message" class="contact__label">Message</label>
-                <textarea name="message" id="contact__form-message" cols="0" rows="7" class="contact__input" minlength="150" maxlength="2000" required></textarea>
+                <textarea name="message" id="contact__form-message" cols="1" rows="7" class="contact__input" minlength="150" maxlength="2000" required></textarea>
                 <small></small>
             </div>
             <?php if(isset($_SESSION['error-message']))
